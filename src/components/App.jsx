40b5270610bfaa1cb666d6635929ba5bc8e165f7
Maryna Pokhyliuk <marinapohiluk19@gmail.com>
2022-10-06 22:23:20 +0300

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import { Profile } from './Profile';
+import { Statistic } from './Statistics';
+import user from './user.json';
+import data from './data.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile profile={user} />
+      <Statistic statistic={data} />
     </div>
   );
 };
